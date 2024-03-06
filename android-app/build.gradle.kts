3a34753e065aa50ceca156eb87e8ccff68634afa
Teodor Grigor <open-source@teogor.dev>
2024-03-06 16:04:31 +0200

    Update build.gradle.kts

--- android-app/build.gradle.kts
+++ android-app/build.gradle.kts
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 import dev.teogor.xenoglot.Country
 import dev.teogor.xenoglot.Language
 import dev.teogor.xenoglot.territorialize
